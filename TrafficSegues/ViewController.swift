f286f440c0af31888f42a878016d08ac134bb50f
Rey Aponte <reynaldo.apontedavila@montgomerycollege.edu>
2022-06-23 14:20:40 -0400

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Aponte, Raynaldo E on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
