77a4959c0b24a51a17d13c140b812bbc026c2861
Rey Aponte <reynaldo.apontedavila@montgomerycollege.edu>
2022-07-12 15:43:01 -0400

    created programmatic segues

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,18 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-        
-    }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = redTextField.text
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
     }
 }
 
