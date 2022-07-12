91e88efabca2a40046376319c207d8aa6e1f9bc2
Rey Aponte <reynaldo.apontedavila@montgomerycollege.edu>
2022-07-12 11:03:47 -0400

    Passing information forward

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,6 +19,8 @@ class ViewController: UIViewController {
         
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = redTextField.text
+    }
 }
 
